bf2039bf9d01bec1e4df812b05d6aabc9524004a
Marcos Sugai Ogama <pazzino2000@yahoo.it>
2022-07-13 09:28:45 -0300

    final aula Componente de função

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
 
+import Cesta from './src/telas/Cesta';
+
 export default function App() {
   return (
     <View style={styles.container}>
